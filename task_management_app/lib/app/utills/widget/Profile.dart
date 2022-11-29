5fdc7de8918d5841d1f644076bc1bab9ff08d559
SnurAulia <snuraulia48@gmail.com>
2022-11-29 19:18:23 +0700

    Video 11 - Firebase Config Android & web

--- task_management_app/lib/app/utills/widget/Profile.dart
+++ task_management_app/lib/app/utills/widget/Profile.dart
@@ -61,7 +61,7 @@ class Profile extends StatelessWidget {
                   ClipRRect(
                     child: CircleAvatar(
                       backgroundColor: Colors.amber,
-                      radius: 150,
+                      radius: 100,
                       foregroundImage: NetworkImage(
                           'https://th.bing.com/th/id/R.4423b396783ee5e3c974c3f42d31a5d2?rik=B2TGBxm6KvE62Q&riu=http%3a%2f%2fi1.hdslb.com%2fbfs%2farchive%2f74188e89512904b3c99db99b5cd7eb4ed6876e80.jpg&ehk=w8PLQ5hrZzAC2zHbr2kFvq7R7YsE5GWxGBw6tP9KFis%3d&risl=&pid=ImgRaw&r=0'),
                     ),
@@ -70,7 +70,7 @@ class Profile extends StatelessWidget {
                     height: 20,
                   ),
                   Text(
-                    'Alicia Jasmin',
+                    'Alicia Jasmine',
                     style: TextStyle(
                       color: AppColors.primarytext,
                       fontSize: 40,
